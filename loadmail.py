6d0e2070b4350cc26813a6eaead51aa13d79a0ad
PhysicalChief <alexsoldat98@ggmail.com>
2019-07-07 06:40:00 +0300

    create list_mail first from second message

--- loadmail.py
+++ loadmail.py
@@ -16,29 +16,60 @@ def get_first_text_block(email_message_instance):
     elif maintype == 'text':
         return email_message_instance.get_payload()
 
+def get_list_email():
+    list_mail = []
+    list_text = []
+    list_from = []
+    conn = imaplib.IMAP4_SSL("imap.yandex.ru", 993)
+    conn.login("guap4636@yandex.ru", "4636guap")
+    conn.select()
+    typ, data = conn.search(None, 'Unseen')  # Unseen для непрочитанных
+    try:
+        for num in data[0].split():
+            typ, msg_data = conn.fetch(num, '(RFC822)')
+            for response_part in msg_data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_bytes(response_part[1])
+                    payload = msg.get_payload()
+                    body = extract_body(payload)
+                    From = msg['from']
+                    Text = get_first_text_block(msg)
+                    list_text.append(Text)
+                    list_from.append(From)
+            typ, response = conn.store(num, '+FLAGS', r'(\Seen)') #пометка сообщения как прочитанного
+    finally:
+        try:
+            conn.close()
+        except:
+            pass
+        conn.logout()
+        list_mail.append(list_from)
+        list_mail.append(list_text)
+        return list_mail
+
 #в body выводится текст сообщения
 #в From отправитель
-conn = imaplib.IMAP4_SSL("imap.yandex.ru", 993)
-conn.login("guap4636@yandex.ru", "4636guap")
-conn.select()
-typ, data = conn.search(None, 'ALL') #Unseen для непрочитанных
-try:
-    for num in data[0].split():
-        typ, msg_data = conn.fetch(num, '(RFC822)')
-        for response_part in msg_data:
-            if isinstance(response_part, tuple):
-                msg = email.message_from_bytes(response_part[1])
-                payload=msg.get_payload()
-                body=extract_body(payload)
-                print(body)
-                From = msg['from']
-                print(From)
-                Text = get_first_text_block(msg)
-                print(Text)
-        #typ, response = conn.store(num, '+FLAGS', r'(\Seen)') пометка сообщения как прочитанного
-finally:
-    try:
-        conn.close()
-    except:
-        pass
-    conn.logout()
+#conn = imaplib.IMAP4_SSL("imap.yandex.ru", 993)
+#conn.login("guap4636@yandex.ru", "4636guap")
+#conn.select()
+#typ, data = conn.search(None, 'ALL') #Unseen для непрочитанных
+#try:
+#    for num in data[0].split():
+#        typ, msg_data = conn.fetch(num, '(RFC822)')
+#        for response_part in msg_data:
+#            if isinstance(response_part, tuple):
+#                msg = email.message_from_bytes(response_part[1])
+#                payload=msg.get_payload()
+#                body=extract_body(payload)
+#                print(body)
+#                From = msg['from']
+#                print(From)
+#                Text = get_first_text_block(msg)
+#                print(Text)
+#        #typ, response = conn.store(num, '+FLAGS', r'(\Seen)') пометка сообщения как прочитанного
+#finally:
+#    try:
+#        conn.close()
+#    except:
+#        pass
+#    conn.logout()
