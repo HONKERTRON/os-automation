e1a8de7f9d06dbdf697dbb7a35c36118cf2ebf93
PhysicalChief <alexsoldat98@ggmail.com>
2019-07-07 05:22:26 +0300

    create

---
+++ loadmail.py
@@ -0,0 +1,30 @@
+import imaplib
+import email
+
+def extract_body(payload):
+    if isinstance(payload,str):
+        return payload
+    else:
+        return '\n'.join([extract_body(part.get_payload()) for part in payload])
+
+#в body выводится текст сообщения
+conn = imaplib.IMAP4_SSL("imap.yandex.ru", 993)
+conn.login("guap4636@yandex.ru", "4636guap")
+conn.select()
+typ, data = conn.search(None, 'ALL') #Unseen для непрочитанных
+try:
+    for num in data[0].split():
+        typ, msg_data = conn.fetch(num, '(RFC822)')
+        for response_part in msg_data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_bytes(response_part[1])
+                payload=msg.get_payload()
+                body=extract_body(payload)
+                print(body)
+        #typ, response = conn.store(num, '+FLAGS', r'(\Seen)') пометка сообщения как прочитанного
+finally:
+    try:
+        conn.close()
+    except:
+        pass
+    conn.logout()
